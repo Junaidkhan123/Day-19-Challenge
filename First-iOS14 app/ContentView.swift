a4b21a2e580f88f7786fdf74365410349b737730
Muhammad  Junaid <junaid123.jk78@gmail.com>
2020-06-27 18:48:36 +0500

    Initial Commit

---
+++ First-iOS14 app/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  First-iOS14 app
+//
+//  Created by Jhon Khan on 27/06/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ First-iOS14 app/First_iOS14_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_iOS14_appApp.swift
+//  First-iOS14 app
+//
+//  Created by Jhon Khan on 27/06/2020.
+//
+
+import SwiftUI
+
+@main
+struct First_iOS14_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
