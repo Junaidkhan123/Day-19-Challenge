661b0151cff5c75a8c4bc2be43bfcad9e913a9f3
Muhammad  Junaid <junaid123.jk78@gmail.com>
2020-06-28 11:18:26 +0500

    Completed Challenge

--- First-iOS14 app/ContentView.swift
+++ First-iOS14 app/ContentView.swift
@@ -8,8 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
+    @State private var inputUnit = 0
+    @State private var outputUnits = 0
+    let inputUnits = ["Day", "Hours", "Minutes", "Seconds"]
+    
+    var convertedValue: Double {
+        let value        = Double(name) ?? 0.0
+        if inputUnit == 0 {
+            if outputUnits == 1 {
+                return 24  * value
+            }
+            else if outputUnits == 0 {
+                return value
+            }
+            else if outputUnits ==  2 {
+                return 24 * 60  * value
+            }
+            else {
+                return 24 * 60 * 60  * value
+            }
+        }
+        else if inputUnit == 1 {
+            if outputUnits == 1 {
+                return value
+            }
+            else if outputUnits == 0 {
+                return value / 24
+            }
+            else if outputUnits ==  2 {
+                return 60  * value
+            }
+            else {
+                return 60 * 60  * value
+            }
+        }
+
+        return 0.0
+    }
+    
     var body: some View {
-        Text("Hello, world!").padding()
+        NavigationView{
+            Form{
+                
+                Section{
+                    TextField("Enter Value", text: $name)
+                }
+                
+                Section(header: Text("Choose Input Unit")){
+                    Picker("Units", selection: $inputUnit) {
+                        ForEach(0..<inputUnits.count){ eachUnit in
+                            Text("\(inputUnits[eachUnit])")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Choose Output Unit")){
+                    Picker("Units", selection: $outputUnits) {
+                        ForEach(0..<inputUnits.count){ eachUnit in
+                            Text("\(inputUnits[eachUnit])")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Converted Value")) {
+                    Text("\(convertedValue, specifier: "%.2f")")
+                }
+
+            }
+            .navigationBarTitle("Converter")
+        }
     }
 }
 
